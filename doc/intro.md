d96ab5a35e524eb0aa5de97a4a03ae7da3d440dc
Sander Kolman <sanderkolman@gmail.com>
2018-02-19 12:33:49 +0100

    project stuff

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to site-diff-checker
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/site_diff_checker/core_test.clj
@@ -0,0 +1,7 @@
+(ns site-diff-checker.core-test
+  (:require [clojure.test :refer :all]
+            [site-diff-checker.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
