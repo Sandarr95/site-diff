5c9b98fe03e7c1879d1931b5750a8a0e9ad03f4e
Sander Kolman <sanderkolman@gmail.com>
2018-02-19 12:33:03 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject site-diff-checker "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot site-diff-checker.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
